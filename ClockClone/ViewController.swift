f80424417175d9fb0b09bf16d1d34c510f2ec37c
Lacie <dark830917@gmail.com>
2019-10-22 09:20:46 +0800

    Initial Commit

---
+++ ClockClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClockClone
+//
+//  Created by 黃士軒 on 2019/10/22.
+//  Copyright © 2019 Lacie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
