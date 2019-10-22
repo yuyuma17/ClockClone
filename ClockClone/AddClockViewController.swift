cb2ada3477d38b723be2d317fcc33a72087e9d64
Lacie <dark830917@gmail.com>
2019-10-22 15:48:19 +0800

    add new clock page

---
+++ ClockClone/AddClockViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddClockViewController.swift
+//  ClockClone
+//
+//  Created by 黃士軒 on 2019/10/22.
+//  Copyright © 2019 Lacie. All rights reserved.
+//
+
+import UIKit
+
+class AddClockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func cancelAddClock(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func saveNewClock(_ sender: UIBarButtonItem) {
+    }
+    
+
+
+}
