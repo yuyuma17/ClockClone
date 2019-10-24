c5c97e7d435075c623a861382d7113b6c8002eba
Lacie <dark830917@gmail.com>
2019-10-24 14:40:47 +0800

    set up disclosure indicator

--- ClockClone/Controller/SettingTableViewController.swift
+++ ClockClone/Controller/SettingTableViewController.swift
@@ -10,10 +10,26 @@ import UIKit
 
 class SettingTableViewController: UITableViewController {
 
+    @IBOutlet weak var repeatOptionCell: UITableViewCell!
+    @IBOutlet weak var tagOptionCell: UITableViewCell!
+    @IBOutlet weak var voiceOptionCell: UITableViewCell!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.tableFooterView = UIView()
+        
+        repeatOptionCell.accessoryView = setAccessoryView() as? UIView
+        tagOptionCell.accessoryView = setAccessoryView() as? UIView
+        voiceOptionCell.accessoryView = setAccessoryView() as? UIView
+    }
+    
+
+    
+    func setAccessoryView() -> Any {
+        let indicator = UIImageView(frame: CGRect(x: 0, y: 0, width: 10, height: 16))
+        indicator.image = UIImage(named: "indicator")
+        return indicator
     }
 
 }
